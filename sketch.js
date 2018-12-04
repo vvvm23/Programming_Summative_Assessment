2563a3201a3807632ab1d33b016ec73ac7b16a4e
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2018-12-04 16:11:58 +0000

    fixed the fucker up

--- sketch.js
+++ sketch.js
@@ -1,31 +1,41 @@
 class InAndOut {
-  constructor() {
-    let t=0.001;
+  constructor(x_pos=40, y_pos=460, length=460, level=100, colour=[0,0,255], colour_fade=[3,0,-3], line_weight=1, animation_time=12000, master_rotate=3000) {
+      /*
+        InAndOut.constructor() arguments:
+        x_pos = x position of bottom left corner of triangle. Default = 40
+        y_pos = y position of bottom left corner of triangle. Default = 460
+        length = Size of one side of triangle. Default = 460
+        level = Recursion Depth of Triangles. Default = 100
+        colour = Triangle Line colour. Default = [0, 0, 0]
+        colour_fade = Add value to colour elementwise every recursive call. Default = [5, 0, -5]
+        line_weight = Thickness of drawn line. Default = 3
+        animation_time = Time for one complete loop of the animation in milliseconds. Default = 6000
+        master_rotate = Rotational speed for entire object. Default = 0
+      */
+    this.x_pos = x_pos;
+    this.y_pos = y_pos;
+    this.length = length;
+    this.level = level;
+    this.colour = colour;
+    this.colour_fade = colour_fade;
+    this.line_weight = line_weight;
+    this.animation_time = animation_time;
+    this.master_rotate = master_rotate;
   }
 
-  draw(x_pos=40, y_pos=460, length=460, level=100, colour=[0,0,255], colour_fade=[5,3,-5], line_weight=3, animation_time=12000, master_rotate=3000)
+  draw()
   {
-    /*
-      InAndOut.draw() arguments:
-      x_pos = x position of bottom left corner of triangle. Default = 40
-      y_pos = y position of bottom left corner of triangle. Default = 460
-      length = Size of one side of triangle. Default = 460
-      level = Recursion Depth of Triangles. Default = 100
-      colour = Triangle Line colour. Default = [0, 0, 0]
-      colour_fade = Add value to colour elementwise every recursive call. Default = [5, 0, -5]
-      line_weight = Thickness of drawn line. Default = 3
-      animation_time = Time for one complete loop of the animation in milliseconds. Default = 6000
-      master_rotate = Rotational speed for entire object. Default = 0
-    */
+    let colour = this.colour;
+    let level = this.level;
     stroke(colour);
-    strokeWeight(line_weight);
-    let animation_ratio = PI/animation_time;
+    strokeWeight(this.line_weight);
+    let animation_ratio = PI/this.animation_time;
 
-    translate(x_pos+length/2, y_pos - 0.5*(length/2 * tan(PI/3)));
-    rotate(PI/master_rotate*millis());
-    translate(-(x_pos+length/2), -y_pos + 0.5*(length/2 * tan(PI/3)))
+    translate(this.x_pos+this.length/2, this.y_pos - 0.5*(this.length/2 * tan(PI/3)));
+    rotate(PI/this.master_rotate*millis());
+    translate(-(this.x_pos+this.length/2), -this.y_pos + 0.5*(this.length/2 * tan(PI/3)))
 
-    this.harom(x_pos + length, y_pos, x_pos, y_pos,level,(sin(animation_ratio*millis()%(2*PI))+1)/2, colour, colour_fade);
+    this.harom(this.x_pos + this.length, this.y_pos, this.x_pos, this.y_pos, level, (sin(animation_ratio*millis()%(2*PI))+1)/2, colour, this.colour_fade);
   }
 
   harom(ax, ay, bx, by, level, ratio, colour, colour_fade)
@@ -46,7 +56,6 @@ class InAndOut {
     n_colour[0] = colour[0] + colour_fade[0]
     n_colour[1] = colour[1] + colour_fade[1]
     n_colour[2] = colour[2] + colour_fade[2]
-    //console.log(n_colour)
 
     this.harom(ax*ratio+cx*(1-ratio),ay*ratio+cy*(1-ratio),ax*(1-ratio)+bx*ratio,ay*(1-ratio)+by*ratio,level-1,ratio, n_colour, colour_fade);
     }
@@ -54,8 +63,8 @@ class InAndOut {
 }
 
 let i = []
-for (let _ = 0; _<10; _++) {
-  i.push(new InAndOut(x_pos=_*30, y_pos=460, length=460, line_weight=1));
+for (let _ = 0; _<1; _++) {
+  i.push(new InAndOut());
 }
 
 function setup()
@@ -66,7 +75,7 @@ function setup()
 function draw()
 {
   background(255);
-  for (let _ = 0; _<10; _++) {
+  for (let _ = 0; _<1; _++) {
     i[_].draw();
   }
 }
